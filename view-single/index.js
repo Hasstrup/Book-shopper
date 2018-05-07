71f2304ad80624570d4f34243aa78c4417087b52
CHIMA CHUKWUEMEKA EMMANUEL <chukwuemekachm@gmail.com>
2018-05-07 14:59:13 +0100

    Refactored code to 2 spaces instead of 4

--- view-single/index.js
+++ view-single/index.js
@@ -6,39 +6,39 @@ const baseUrl = 'http://fakerestapi.azurewebsites.net';
  * based on the id argument
  */
 const viewBook = (bookId) => {
-    let book = {};
-    let status = true;
-    fetch(`${baseUrl}/api/Books/${bookId}`, { method: 'GET' })
-        .then(response => response.json())
-        .then((data) => {
-            const bookResponse = data;
-            book.Id = bookResponse.ID;
-            book.Title = bookResponse.Title;
-            book.Description = bookResponse.Description;
-            book.PageCount = bookResponse.PageCount;
-            book.Excerpt = bookResponse.Excerpt;
-            book.PublishDate = bookResponse.PublishDate;
-        })
-        .catch((err) => {
-            status = false;
-        });
-    fetch(`${baseUrl}/api/coverPhotos/${bookId}`, { method: 'GET' })
-        .then(response => response.json())
-        .then((data) => {
-            const coverPhoto = data;
-            book.Url = coverPhoto.Url;
-        }).catch((err) => {
-            status = false;
-        });
+  let book = {};
+  let status = true;
+  fetch(`${baseUrl}/api/Books/${bookId}`, { method: 'GET' })
+      .then(response => response.json())
+      .then((data) => {
+      	const bookResponse = data;
+        book.Id = bookResponse.ID;
+        book.Title = bookResponse.Title;
+        book.Description = bookResponse.Description;
+        book.PageCount = bookResponse.PageCount;
+        book.Excerpt = bookResponse.Excerpt;
+        book.PublishDate = bookResponse.PublishDate;
+      })
+      .catch((err) => {
+        status = false;
+      });
+  fetch(`${baseUrl}/api/coverPhotos/${bookId}`, { method: 'GET' })
+      .then(response => response.json())
+      .then((data) => {
+          const coverPhoto = data;
+          book.Url = coverPhoto.Url;
+      }).catch((err) => {
+          status = false;
+      });
     /**
      * return a book of a false boolean value
      * based on the status boolean value
      */
-    if (status) {
-        return book;
-    } else {
-        return false;
-    }
+  if (status) {
+      return book;
+  } else {
+      return false;
+  }
 };
 
 /**
@@ -46,8 +46,8 @@ const viewBook = (bookId) => {
  * based on the id argument
  */
 const removeBookAuthor = (bookAuthorId) => {
-    fetch(`${baseUrl}/api/Authors/${bookAuthorId}`, { method: 'DELETE' })
-        .then(response => response).then(data => true).catch(err => false);
+  fetch(`${baseUrl}/api/Authors/${bookAuthorId}`, { method: 'DELETE' })
+      .then(response => response).then(data => true).catch(err => false);
 };
 
 export { viewBook, removeBookAuthor };
